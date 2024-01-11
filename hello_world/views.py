1b81fb8cbe1e4041c15ed5a6a1536798fb4cda90
Georgios Kopanidis <kopanidis@me.com>
2024-01-11 14:05:40 +0000

    Final commit

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    
     if request.method == "POST":
         return HttpResponse("You must have POSTed something")
     else:
